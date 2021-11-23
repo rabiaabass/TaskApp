628ee2896b9ea9be91a083e07826141e1df96d06
rabiaabass <bassrabiaa@gmail.com>
2021-11-23 20:02:05 +0300

    Opened ticket service

---
+++ TaskApp/Services/TicketServices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskApp.Models;
+using TaskApp.Repositories;
+
+namespace TaskApp.Services
+{
+    public class TicketServices
+    {
+        TicketRepository _ticketRepository;
+
+        public TicketServices(TicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
